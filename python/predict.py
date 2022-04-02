30d1982a0c26e63647aeff0e897ede5042f2ad7c
Shihan Liu <69880398+LSH9832@users.noreply.github.com>
2022-04-02 13:56:48 +0800

    Update predict.py

--- python/predict.py
+++ python/predict.py
@@ -24,29 +24,35 @@ def show(my_dict, detector, source):
         if success:
 
             frames.append(frame)
-
+            
+            # send image data
             my_dict['img'] = detector.preprocess(frame, detector.get_input_size())
             my_dict['updated'] = True
-
+            
+            # count fps
             fpss.append(detector.fps())
             fpss = fpss[1:] if len(fpss) > 10 else fpss
             now_mean_fps = sum(fpss) / len(fpss)
             print('\rplay_fps=%.2f, inference_fps=%.2f' % (now_mean_fps, my_dict['pre_fps']), end='')
-
+            
+            # get last frame
             while len(frames) > 1:
                 frame = frames.pop(0)
-
+            
+            # decode result
             if my_dict['update_result']:
                 result = my_dict['result']
                 my_dict['update_result'] = False
                 if result is not None and len(result):
                     result = detector.postprocess(result, detector.get_input_size())
+            
+            # plot result
             if result is not None and len(result):
                 detector.plot_result(frame, result)
 
-            cv2.imshow('test', frame)
+            cv2.imshow('test', frame)       # show image
 
-            if cv2.waitKey(1) == 27:
+            if cv2.waitKey(1) == 27:        # press esc and quit
                 cam.release()
                 break
         else:
@@ -55,10 +61,12 @@ def show(my_dict, detector, source):
             except:
                 pass
             if is_webcam:
+                # try reconnect
                 cam.open(source)
             else:
                 break
-
+        
+        # control video play speed
         # while not time() - t1 >= 0.03:
         #     pass
 
@@ -70,15 +78,15 @@ def show(my_dict, detector, source):
 def detect(my_dict, detector):
     detector.load()
     my_dict['run'] = True
-    while my_dict['run']:                               # 如果程序仍需要运行
-        if my_dict['updated']:                          # 如果图像已经更新
-            img = my_dict['img']                        # 获取图像
-            my_dict['updated'] = False                  # 设置图像状态为未更新
-            detector.fps()                              # 开始计时
-            result = detector.inference(img)            # 推理
-            my_dict['pre_fps'] = detector.fps()         # 结束计时并计算FPS
-            my_dict['result'] = result                  # 存储结果
-            my_dict['update_result'] = True             # 设置结果状态为已更新
+    while my_dict['run']:                               # run flag
+        if my_dict['updated']:                          # whether updated
+            img = my_dict['img']                        # get image
+            my_dict['updated'] = False                  # set updated false
+            detector.fps()                              # time starts
+            result = detector.inference(img)            # inference
+            my_dict['pre_fps'] = detector.fps()         # time over
+            my_dict['result'] = result                  # save result
+            my_dict['update_result'] = True             # set result_update true
 
 
 class Predicter:
@@ -88,25 +96,25 @@ class Predicter:
         self.__detector = detector
 
     def run_single(self):
-        self.__detector.load()
-        cam = cv2.VideoCapture(self.__source)
-        self.__detector.fps()
+        self.__detector.load()                                      # load model
+        cam = cv2.VideoCapture(self.__source)                       # open image source
+        self.__detector.fps()                                       # calculate fps
         while cam.isOpened():
-            success, image = cam.read()
+            success, image = cam.read()                             # read image
             if success:
 
                 # get and plot result
-                result = self.__detector.predict(image)
+                result = self.__detector.predict(image)             # inference and get result
                 if result is not None and len(result):
-                    self.__detector.plot_result(image, result)
+                    self.__detector.plot_result(image, result)      # plot result in the origion image
 
                 # show
-                cv2.imshow("result", image)
+                cv2.imshow("result", image)                         # show image
                 if cv2.waitKey(1) == 27:
                     cv2.destroyAllWindows()
                     cam.release()
                     break
-            print("\r%.2f fps" % self.__detector.fps(), end="")
+            print("\r%.2f fps" % self.__detector.fps(), end="")     # print fps
         print("")
 
     def run_multiple(self):
